79082634adb070894a5996286b4a5b98703b6476
BurAndBY <48630651+BurAndBY@users.noreply.github.com>
2022-08-20 20:34:04 +0300

    Дейл Лох

--- main.js
+++ main.js
@@ -4610,7 +4610,7 @@
                     return this.htmlEscape(t).trim()
                 }
                 static sanitizeName(e) {
-                    return e.replace(/[^A-Z0-9\u00A1\u0020-\u002F\u00BF-\u00FF\u2026!?*$+\-'_ .,]/gi, "").replace(/'/g, "’")
+                    return e.replace(/[^A-Z0-9\u0400-\u04FF\u00A1\u0020-\u002F\u00BF-\u00FF\u2026!?*$+\-'_ .,]/gi, "").replace(/'/g, "’")
                 }
                 static sanitizeInput(e) {
                     return e.replace(/[^\u00A1\u0020-\u007E\u00BF-\u00FF’]/gi, "")
